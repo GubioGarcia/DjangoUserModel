1d870ce677c2a15523a0d24bfc7d3daee13260cb
Gubio Garcia <gubiogarcia@gmail.com>
2024-01-11 11:30:39 -0300

    Feat: limitação de visualização na seção admin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from .models import Post
 
 @admin.register(Post)
@@ -7,3 +10,8 @@ class PostAdmin(admin.ModelAdmin):
 
     def _autor(autor, instance):
         return f'{instance.autor.get_full_name()}'
+    
+    # função sobrescrita que limita a visualização dos posts apenas ao usuário logado
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(autor=request.user)
