449472a06f7c4ca56ea4ca0ec902e896afe75db5
Gubio Garcia <gubiogarcia@gmail.com>
2024-01-11 11:40:09 -0300

    Feat: seleção automatica do autor

--- core/admin.py
+++ core/admin.py
@@ -7,6 +7,7 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'autor')
+    exclude = ['autor',]
 
     def _autor(autor, instance):
         return f'{instance.autor.get_full_name()}'
@@ -15,3 +16,8 @@ class PostAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super(PostAdmin, self).get_queryset(request)
         return qs.filter(autor=request.user)
+    
+    # anterior ao save, recupera o autor logado e salva automaticamente no objeto
+    def save_model(self, request, obj, form, change):
+        obj.autor = request.user
+        super().save_model(request, obj, form, change)
